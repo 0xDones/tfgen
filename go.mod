546d85df4c47a4352d045b73d238d0af6000554a
Denis Policastro <denis.policastro@gmail.com>
2022-01-29 21:50:05 -0300

    feat: transform tfgen in a cli app

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module tfgen
 go 1.17
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/urfave/cli/v2 v2.3.0 // indirect
+	github.com/sirupsen/logrus v1.8.1
+	github.com/urfave/cli/v2 v2.3.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"tfgen/config"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
 
-	log.SetLevel(log.FatalLevel)
+	log.SetLevel(log.ErrorLevel)
 }
 
 func main() {
-	// TODO: Add CLI
-	workingDir := "."
-	configs, err := config.GetConfigFiles(workingDir)
+	app := &cli.App{
+		Name:  "tfgen",
+		Usage: "Terraform boilerplate generator",
+		Commands: []*cli.Command{
+			{
+				Name:      "exec",
+				Usage:     "execute tfgen to generate the files",
+				ArgsUsage: "<working dir>",
+				Action: func(c *cli.Context) error {
+					workingDir := c.Args().First()
+					fmt.Printf("Working Dir: %s\n", workingDir)
+					return exec(workingDir)
+				},
+			},
+		},
+		CommandNotFound: func(c *cli.Context, command string) {
+			println(fmt.Sprintf("error: no matching command '%s'", command))
+			cli.ShowAppHelp(c)
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func exec(workingDir string) error {
+	// Check if workindDir is directory and exists
+	if dir, err := os.Stat(workingDir); os.IsNotExist(err) || !dir.IsDir() {
+		return fmt.Errorf("path '%s' is not a directory or doesn't exist", workingDir)
+	}
+
+	configs, err := config.GetConfigFiles(workingDir)
+	if err != nil {
+		return err
+	}
 
 	mergedConfig := config.MergeAll(configs)
-	// println(fmt.Sprintf("Final config: %+v", mergedConfig))
 	println("tfgen created all the files successfully")
 	mergedConfig.WriteFiles()
+	return nil
 }
