b12511bc3060ce00b82f875146fb21c9c88e99dc
ndunn990 <nickdunn@carrierpigeon.email>
2022-07-07 16:11:43 -0400

    New Commands: 'clean' and 'clean_all'
    New commands that will allow tfgent to delete files that were generated
    by a template. These commands expect a 'clean_pattern' to be defined,
    ideally in the root configuration to allow for the 'clean_all' command,
    which will clean the 'child_directories' defined in the root confing.
    This should give the user a great deal of control around how they want
    things cleaned up when needed.

---
+++ cmd/clean_all.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+
+	"path"
+
+	"tfgen/config"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCleanAllCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "clean_all <target directory>",
+		Short: "Clean up generated template files in all specified child directories.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			targetDir := args[0]
+			return clean_all(targetDir)
+		},
+	}
+
+}
+
+func clean_all(targetDir string) error {
+	rootConfig, err := config.GetRootConfig(targetDir)
+	if err != nil {
+		return err
+	}
+	if rootConfig.CleanPattern == "" {
+		return fmt.Errorf("Unable to clean templated files -- no 'clean_pattern' has been defined in root config.")
+	}
+	for _, childDir := range rootConfig.ChildDirectories {
+		err := config.CleanTemplateFiles(path.Join(targetDir, childDir))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
